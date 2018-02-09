c2445c2bf4c59e9cb16cf012762eda8107032c6f
201101050424 <xgqxgqxx@gmail.com>
2018-02-09 14:51:51 +0800

    add optim

---
+++ pytorch/optim/optim_note.py
@@ -0,0 +1,20 @@
+#encoding=utf-8
+import torch
+import torch.optim as optim
+import time
+from torch.autograd import Variable
+
+param=Variable(torch.FloatTensor([[1], [2]]), requires_grad=True)
+
+# 把需要优化的参数传递给优化器
+optimizer=optim.SGD([param], lr=1e-1, momentum=0.9)
+step=0
+while True:
+    optimizer.zero_grad()
+    out=torch.mul(param, param).sum()
+    # 求梯度
+    out.backward()
+    optimizer.step()
+    print 'step=%s value=%f' % (step,out.data[0])
+    step+=1
+    time.sleep(0.1)
