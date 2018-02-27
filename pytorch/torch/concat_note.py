c16d0f38795871699bbdcb5ca65f312f1509df18
xueguoqing01 <xueguoqing01@baidu.com>
2018-02-27 22:24:08 +0800

    modify

--- pytorch/torch/concat_note.py
+++ pytorch/torch/concat_note.py
@@ -1,5 +1,7 @@
+#encoding=utf-8
 import torch
 import numpy as np
+from torch.autograd import Variable
 
 # http://pytorch.org/docs/master/torch.html
 
@@ -33,4 +35,22 @@ print torch.cat([a, b], 1)
 1  2  1  2  3
 3  4  3  4  6
 [torch.LongTensor of size 2x5]
+"""
+
+# 对Variable同样适用
+a = Variable(torch.from_numpy(np.array([[1, 2], [3, 4]])))
+b = Variable(torch.from_numpy(np.array([[1, 2, 3], [3, 4, 6]])))
+print torch.cat([a, b], 1)
+"""
+Variable containing:
+1  2  1  2  3
+3  4  3  4  6
+[torch.LongTensor of size 2x5]
+"""
+
+a = torch.Tensor([[1,2], [3,4]]).long()
+b = torch.Tensor([[1,2], [3,4]]).float()
+print torch.cat([a, b], 1)
+"""
+类型不一样会报错
 """
